dae2546641783f6efa6f73501885428fbd6147aa
gulshan25 <g.r.tanny@gmail.com>
2021-12-11 17:02:31 +0600

    OwnWork

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,67 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.red[600],
-          title: Text('MyApp'),
-          centerTitle: true,
-        ),
-        body: Center(
+      home: Home(),
+    ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text('Home'),
+        centerTitle: true,
+      ),
+      body: Container(
+        child: Center(
           child: Text(
-            'Hello World',
+            'Hello World!',
             style: TextStyle(
-              fontSize: 20.0,
+              fontSize: 50.0,
               fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.grey[600],
-              fontFamily: 'IndieFlower',
+              color: Colors.blue,
+              letterSpacing: 1.0,
             ),
           ),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Text('Click'),
-          backgroundColor: Colors.red[600],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('');
+        },
+        backgroundColor: Colors.blue,
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
         ),
       ),
-    ));
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.blue[700],
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              color: Colors.white,
+            ),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
+            label: 'Add',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.search,
+              color: Colors.white,
+            ),
+            label: 'Search',
+          ),
+        ],
+      ),
+    );
+  }
+}
